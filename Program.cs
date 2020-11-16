96d9a66d8a322551aefede133e0b6e82a84d6eac
Thiago <thiagocostas2017@gmail.com>
2020-11-16 13:59:14 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Estrutura_condicional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//Entrada
+            Console.WriteLine("Digite sua idade");
+            int idade = int.Parse(Console.ReadLine());
+            //Processamento
+            int meses = idade * 12;
+            int dias = idade * 365;
+            int horas = dias *24;
+            int minutos = horas * 60;
+
+            Console.WriteLine("Você viveu");
+            Console.WriteLine(meses + "Meses");
+            Console.WriteLine(dias + "Dias");
+            Console.WriteLine(horas + "Horas");
+            Console.WriteLine(minutos + "Minutos");
+            
+        
+        
+        }
+    }
+
+}        
